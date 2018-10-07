ac21133433db8543b63e9950f66c1623872746c8
ymgn <b4183@oic.jp>
2018-10-07 13:35:28 +0900

    Spring Boot

---
+++ src/main/kotlin/com/example/twittermodoki/TwitterModokiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.twittermodoki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TwitterModokiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TwitterModokiApplication>(*args)
+}
