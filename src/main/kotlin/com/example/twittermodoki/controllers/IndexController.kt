2199d5eba8f7057a8781357d85b8cfd788969dd1
ymgn <b4183@oic.jp>
2018-10-14 18:59:15 +0900

    add ユーザー名固定だが呟けるように

--- src/main/kotlin/com/example/twittermodoki/controllers/IndexController.kt
+++ src/main/kotlin/com/example/twittermodoki/controllers/IndexController.kt
@@ -4,9 +4,12 @@ import com.example.twittermodoki.domain.Tweet.Tweet
 import com.example.twittermodoki.usecases.GetTweets
 import com.example.twittermodoki.usecases.RegistrationTweet
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class IndexController(
@@ -15,15 +18,28 @@ class IndexController(
         )
 {
     // TOP
-    @RequestMapping("/")
+    @GetMapping("/")
     fun index(model: Model): String {
-        // 投稿実装する前に表示されるように
-        val tweet1 = Tweet("本文","ymgn")
-        registrationTweet.registrationTweet(tweet1)
-
         // 全てのTweetを取得する
         val tweets = getTweets.get()
         model.addAttribute("tweets", tweets)
         return "index"
+    }
+
+    @PostMapping("/tweet",
+    produces = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    fun registTweet(model: Model, @RequestParam requestParam: Map<String,String>): String {
+
+        val tweetMessage: String = requestParam["message"] ?: ""
+
+        if(tweetMessage.isEmpty()) {
+            // エラーのメッセージだしたい
+            return "redirect:/"
+        }
+
+        val tweet = Tweet(tweetMessage,"ymgn")
+        registrationTweet.registrationTweet(tweet)
+
+        return "redirect:/"
     }
 }
